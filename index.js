0e3cdb93cc4569bd66aeceab7751b2ca7348fbe2
swaraj kadam <swarajkadam45@gmail.com>
2024-04-10 15:34:33 +0530

    try catche addded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import { dbConnection } from "./config/dbConnection.js";
 import {
   checkForAuthentication,
-  restrictTo
+  restrictTo,
 } from "./middleware/authMiddleware.js";
 import cookieParser from "cookie-parser";
 import path from "path";
@@ -13,15 +13,18 @@ import { staticRouter } from "./routes/staticRouter.js";
 import { userRouter } from "./routes/userRouter.js";
 const app = express();
 const PORT = process.env.PORT || 8000;
-dbConnection();
+dbConnection().catch((error) => {
+  console.error("Database connection error:", error);
+  // Handle the error, such as shutting down the server or logging the error
+});
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(checkForAuthentication)
+app.use(checkForAuthentication);
 app
-  .use("/url",restrictTo(["NORMAL","ADMIN"]), urlRouter)
+  .use("/url", restrictTo(["NORMAL", "ADMIN"]), urlRouter)
   .use("/user", userRouter)
   .use("/", staticRouter);
 app.listen(process.env.PORT, () => console.log(`listning to port ${PORT}`));
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -7,26 +7,41 @@ export const staticRouter = express.Router();
 staticRouter
   .route("/admin/urls")
   .get(restrictTo(["ADMIN"]), async (req, res) => {
-    const urls = await url.find({}); //user
-    return res.render("urlHome", {
-      urldata: urls,
-    });
+    try {
+      const urls = await url.find({}); //user
+      return res.render("urlHome", {
+        urldata: urls,
+      });
+    } catch (error) {
+      console.error("Error fetching URLs:", error);
+      res.status(500).send("Internal Server Error");
+    }
   });
 
 staticRouter
   .route("/")
   .get(restrictTo(["NORMAL", "ADMIN"]), async (req, res) => {
-    const urls = await url.find({ createdBy: req.userdata._id }); //user
-    return res.render("urlHome", {
-      urldata: urls,
-    });
+    try {
+      const urls = await url.find({ createdBy: req.userdata._id }); //user
+      return res.render("urlHome", {
+        urldata: urls,
+      });
+    } catch (error) {
+      console.error("Error fetching user URLs:", error);
+      res.status(500).send("Internal Server Error");
+    }
   });
 
 staticRouter.route("/signup").get(async (req, res) => {
-  const userDetails = await userSchema.find({});
-  return res.render("userHome", {
-    users: userDetails,
-  });
+  try {
+    const userDetails = await userSchema.find({});
+    return res.render("userHome", {
+      users: userDetails,
+    });
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 staticRouter.route("/login").get(async (req, res) => {
